86f15940d3842a789871c187dfd25daf3a56dd62
Mikael Johansson <mikl.johansson@gmail.com>
2012-04-14 12:01:46 +0200

    Otsu method filtering supported in RenderScript

---
+++ src/se/embargo/onebit/MainActivity.java
@@ -0,0 +1,167 @@
+package se.embargo.onebit;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+
+import se.embargo.core.databinding.DataBindingContext;
+import se.embargo.core.databinding.WidgetProperties;
+import se.embargo.core.databinding.observable.IObservableValue;
+import se.embargo.core.databinding.observable.WritableValue;
+import se.embargo.core.graphics.Bitmaps;
+import se.embargo.onebit.filter.ScriptC_otsu;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
+import android.widget.ImageView;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+public class MainActivity extends SherlockActivity {
+	private static final int CAMERA_RESPONSE_CODE = 1;
+	private static final int GALLERY_RESPONSE_CODE = 2;
+
+	private RenderScript _renderContext;
+	private ScriptC_otsu _filterScript;
+	
+	private DataBindingContext _binding = new DataBindingContext();
+	private IObservableValue<Bitmap> _image = new WritableValue<Bitmap>();
+	private Uri _imageuri = null;
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.main);
+        
+        // Setup the renderscript filters
+        _renderContext = RenderScript.create(this);
+        _filterScript = new ScriptC_otsu(_renderContext, getResources(), R.raw.otsu);
+        
+		final ImageView imageview = (ImageView)findViewById(R.id.image);
+		_binding.bindValue(
+			WidgetProperties.imageBitmap().observe(imageview),
+			_image);
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.main_options, menu);
+        return true;
+	}
+	
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+            case R.id.takePhoto: {
+            	takePhoto();
+	            return true;
+            }
+
+            case R.id.attachImage: {
+	            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+	            intent.setType("image/*");
+	            startActivityForResult(intent, GALLERY_RESPONSE_CODE);
+	            return true;
+            }
+            
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void filterImage(File file) {
+		Bitmap input = Bitmaps.decodeStream(file, 1024, 1024);
+		
+		if (input != null && input.getConfig() != null) {
+			Bitmap output = Bitmap.createBitmap(
+				input.getWidth(), input.getHeight(), input.getConfig());
+			
+	        Allocation inputbuf = Allocation.createFromBitmap(
+	        	_renderContext, input, 
+	        	Allocation.MipmapControl.MIPMAP_NONE, 
+	        	Allocation.USAGE_SCRIPT);
+	        Allocation outputbuf = Allocation.createTyped(
+	        	_renderContext, inputbuf.getType());
+
+	        _filterScript.set_gIn(inputbuf);
+	        _filterScript.set_gOut(outputbuf);
+	        _filterScript.set_gScript(_filterScript);
+	        _filterScript.invoke_filter();
+	        
+	        outputbuf.copyTo(output);
+	        _image.setValue(output);
+		}
+    }
+    
+	private void takePhoto() {
+		FileOutputStream fd;
+		String filename = "1bit.jpg";
+		
+		// Create the temporary file
+		try {
+			fd = openFileOutput(filename, Context.MODE_WORLD_WRITEABLE | Context.MODE_APPEND);
+			fd.close();
+		}
+		catch (IOException e) {
+            new AlertDialog.Builder(this).setMessage(
+            	"Failed to create temporary file '" + filename + "'").setCancelable(true).create().show();
+            return;
+		}
+		
+		// Find out the absolute path to the file
+        File target = new File(getFilesDir() + "/" + filename);
+        _imageuri = Uri.fromFile(target);
+
+        // Send intent to camera app
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageuri);
+        startActivityForResult(intent, CAMERA_RESPONSE_CODE);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			switch(requestCode){
+				case GALLERY_RESPONSE_CODE:
+					Uri contenturi = data.getData();
+					Cursor query = getContentResolver().query(
+						contenturi, 
+						new String[] {MediaStore.MediaColumns.TITLE, MediaStore.MediaColumns.MIME_TYPE, MediaStore.MediaColumns.DATA}, 
+						null, null, null);
+					
+					if (query.moveToFirst()) {
+						String path = query.getString(query.getColumnIndex(MediaStore.MediaColumns.DATA));
+						
+						if (path != null) {
+							File file = new File(path);
+							filterImage(file);
+						}
+					}
+
+					query.close();
+					break;
+
+				case CAMERA_RESPONSE_CODE:
+					if (_imageuri != null) {
+						File file = new File(URI.create(_imageuri.toString()));
+						filterImage(file);
+						_imageuri = null;
+					}
+					break;
+			}
+		}
+	}
+}
---
+++ src/se/embargo/onebit/filter/otsu.rs
@@ -0,0 +1,90 @@
+#pragma version(1)
+#pragma rs java_package_name(se.embargo.onebit.filter)
+
+rs_allocation gIn;
+rs_allocation gOut;
+rs_script gScript;
+
+const static float3 gMonoMult = {0.299f, 0.587f, 0.114f};
+
+void root(const uchar4 *v_in, uchar4 *v_out, const void *usrData, uint32_t x, uint32_t y) {
+	float4 f4 = rsUnpackColor8888(*v_in);
+	float3 mono = dot(f4.rgb, gMonoMult);
+	float3 binary = step(*(const float3 *)usrData, mono);
+	*v_out = rsPackColorTo8888(binary);
+}
+
+void filter() {
+	int64_t t1 = rsUptimeMillis();
+	
+	int32_t width = rsAllocationGetDimX(gIn),
+			height = rsAllocationGetDimY(gIn);
+	int32_t pixels = width * height;
+
+	rsDebug("Otsu image dimensions", width, height);
+
+	// Build the histogram
+	int32_t hist[256] = {0};
+	for (int x = 0; x < width; x++) {
+		for (int y = 0; y < height; y++) {
+			const uchar4 *v_in = (const uchar4 *)rsGetElementAt(gIn, x, y);
+			float4 f4 = rsUnpackColor8888(*v_in);
+			float mono = dot(f4.rgb, gMonoMult);
+			hist[(int)(mono * 255.0f) & 0xff]++;
+		}
+	}
+	
+	// Calculate the global threshold
+	float sum = 0;
+	for (int i = 0; i < 256; i++) {
+		sum += (float)(hist[i] * i);
+	}
+	
+	rsDebug("Otsu histogram sum", sum);
+	
+	float csum = 0;
+	int wB = 0;
+	int wF = 0;
+	
+	float fmax = -1.0;
+	float threshold = 0;
+	
+	for (int i = 0; i < 255; i++) {
+		// Weight background
+		wB += hist[i];
+		if (wB == 0) { 
+			continue;
+		}
+	
+		// Weight foreground
+		wF = pixels - wB;
+		if (wF == 0) {
+			break;
+		}
+	
+		csum += (float)(hist[i] * i);
+	
+		float mB = csum / wB;
+		float mF = (sum - csum) / wF;
+		float sb = (float)wB * (float)wF * (mB - mF) * (mB - mF);
+		//float sb = (float)wB * (float)wF * (mF - mB);
+	
+		// Check if new maximum found
+		if (sb > fmax) {
+			fmax = sb;
+			threshold = i + 1;
+		}
+	}
+	
+	int64_t t2 = rsUptimeMillis();
+	rsDebug("Otsu threshold found in (ms)", t2 - t1);
+	rsDebug("Otsu global threshold", threshold);
+	
+	// Binarize the image
+	float3 usrData = (float)threshold / 255.0f;
+	rsForEach(gScript, gIn, gOut, &usrData);
+
+	int64_t t3 = rsUptimeMillis();
+	rsDebug("Otsu binarization in (ms)", t3 - t2);
+	rsDebug("Filtered image in (ms)", t3 - t1);
+}
